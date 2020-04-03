c746ff8eef2689af3de3fb4e201248d5d3a0e890
julianreinoso94 <julianreinoso94@gmail.com>
2020-04-03 10:02:29 -0300

    Agregando estilos y  funcionalidad al componente Board y Square

--- src/index.js
+++ src/index.js
@@ -4,9 +4,93 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+class Game extends React.Component{
+  render()
+  {
+    return(
+      <div className="game">
+        <div className="game-board">
+          <Board/>
+        </div>
+
+      </div>
+
+    )
+  }
+}
+class Board extends React.Component 
+{
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      squares:Array(9).fill(null),
+    }
+  }
+  renderSquare(i)
+  {
+    return <Square value={this.state.squares[i]}/>;
+  }
+  render(){
+    const status = 'Next Player: X';
+    return (
+    <div>
+          <div className="status">{status}</div>
+          
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+    </div>
+    )
+
+  }
+
+} 
+class Square extends React.Component{
+
+  constructor(props)
+  {
+    super(props);
+    // this.HandleClick = this.HandleClick.bind(this);
+    this.state={
+      value:null,
+    };
+  }
+
+  // HandleClick(e){
+  //   this.setState( )
+  //   e.preventDefault();
+  // }
+
+  render (){
+    return(
+      <button className="square" onClick={()=>this.setState({value:'0'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
